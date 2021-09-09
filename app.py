7f0b8ecf60f5b56e9220c51cdae087ac51599fae
Dan Benson <dbenson3091@gmail.com>
2021-09-09 13:59:14 -0500

    set up survey display on root

--- app.py
+++ app.py
@@ -7,3 +7,24 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+# push satisfaction_survey.questions[question].choices into response lst
+
+#/questions/0
+
+
+@app.get('/')
+def start_survey(): 
+    """Open the survey, retrieve survey.title, and survey.instructions"""
+    title = survey.title 
+    instructions = survey.instructions
+
+    return render_template('/survey_start.html', title=title, instructions=instructions)
+
+@app.get('/questions/<question>')
+def questions_pages(question):
+
+    """Set up docstring"""
+
+    return '' 
